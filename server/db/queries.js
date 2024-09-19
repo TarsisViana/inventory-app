a510d99a1faf5083a5f4ac84733232292f1890cf
tarsis <159029529+TarsisViana@users.noreply.github.com>
2024-09-19 15:08:36 +0100

    Fix proptype errors in the id and amount variables

--- server/db/queries.js
+++ server/db/queries.js
@@ -2,7 +2,7 @@ import pool from "./pool.js";
 
 async function getItems(search) {
   let text =
-    "SELECT itemid, title, price, description, image, categories.name AS category, rating_rate, rating_count FROM inventory JOIN categories ON inventory.category = categoryid";
+    "SELECT itemid AS id, title, price, description, image, categories.name AS category, rating_rate, rating_count FROM inventory JOIN categories ON inventory.category = categoryid";
   if (search) {
     text += " WHERE categoryId = $1";
     var values = [search];
