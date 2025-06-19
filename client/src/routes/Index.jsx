0f073e1fab9e1accdbe4ce751505a5d1dd6154a8
tarsis <159029529+TarsisViana@users.noreply.github.com>
2025-06-19 15:40:33 +0100

    Redirect the home page

--- client/src/routes/Index.jsx
+++ client/src/routes/Index.jsx
@@ -1,11 +1,7 @@
+import {Navigate} from 'react-router-dom'
+
 export default function Index() {
   return (
-    <div style={{
-      margin: "5rem auto",
-      width: "fit-content",
-      textAlign: "center",
-    }}>
-      <h1>Still under construction!</h1>
-    </div>
+    <Navigate to="/products" />
   );
 }
